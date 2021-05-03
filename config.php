ece1debc817bccb577d91422b2f8416fbc264c44
erdaulhaqdani <79428749+erdaulhaqdani@users.noreply.github.com>
2021-05-04 05:29:38 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,16 @@
-<?php 
+<?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "ass2";
+/**
+ * Configuration for database connection
+ *
+ */
 
-$config = mysqli_connect($host, $user, $password, $database);
+$host       = "ass2.mysql.database.azure.com";
+$username   = "erda@ass2";
+$password   = "Danihipya123";
+$dbname     = "ass2";
+$dsn        = "mysql:host=$host;dbname=$dbname";
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
  ?>
